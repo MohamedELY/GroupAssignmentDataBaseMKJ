8432292815cf279580ef0519fb9f7fb9bf5b816b
Kamran Gasimov <kamran.gasimov@edu.newton.se>
2022-02-03 11:37:55 +0100

    first commit

--- GroupAssignmentDataBaseMKJ/Program.cs
+++ GroupAssignmentDataBaseMKJ/Program.cs
@@ -2,11 +2,12 @@
 
 namespace GroupAssignmentDataBaseMKJ
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine( "Very naice");
         }
     }
 }
