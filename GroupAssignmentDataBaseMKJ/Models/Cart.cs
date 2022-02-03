026dc53ae9f2e697966e4fc03572bd1576a08454
Mohamed EL Yahiaoui <Mohamed.el.yahiaoui@outlook.com>
2022-02-03 11:24:34 +0100

    Add project files.

---
+++ GroupAssignmentDataBaseMKJ/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace GroupAssignmentDataBaseMKJ.Models
+{
+    public partial class Cart
+    {
+        public int Id { get; set; }
+        public string CustemerId { get; set; }
+        public int? ProductId { get; set; }
+        public int? Quantity { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
